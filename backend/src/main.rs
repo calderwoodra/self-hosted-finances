d55661aea423767ea477c92e460ef93c771be4ea
Allen Calderwood <allen@allens-mbp.lan>
2022-02-07 12:52:50 -0500

    Removed sample routes

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,22 +6,16 @@ extern crate dotenv;
 
 mod db;
 mod responders;
-mod sample;
 mod users;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .mount(
-            "/sample",
-            routes![sample::controller::index, sample::controller::error],
-        )
-        .mount(
-            "/users",
-            routes![
-                users::controller::get,
-                users::controller::get_all,
-                users::controller::create
-            ],
-        )
+    rocket::build().mount(
+        "/users",
+        routes![
+            users::controller::get,
+            users::controller::get_all,
+            users::controller::create
+        ],
+    )
 }
